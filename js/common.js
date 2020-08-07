8fdb2be9dbac1c96f4c57e5db6d34f23077f9ccd
nickolay.grin <grinmail84@gmail.com>
2020-08-07 18:34:13 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -93,4 +93,30 @@ $(".input-phone").mask("+7 (999) 999-99-99");
 		return false;
 	});
 
+
+
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+if(isMobile.any()) {
+  $(".countdown-wrap").addClass("countdown-wrap_mob");
+}
 });
